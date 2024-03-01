79316c2acc9a7f9f205042ce8c012f07f9047619
eng-omarmo <omarmoo683@gmail.com>
2024-03-01 13:42:13 +0300

    clean up the code added asyn handler

--- Controllers/TaskController.js
+++ Controllers/TaskController.js
@@ -1,111 +1,85 @@
 const Task = require("../model/taskModel");
+const asyncHandler = require("express-async-handler");
 
 //desc  get specific tasks
 //route get :api/tasks
 //access private
-const getTask = async (req, res) => {
-  try {
-    const tasks = await Task.find({ userId: req.userId });
-
-    if (!tasks.length) {
-      return res
-        .status(404)
-        .json({ message: `No tasks found for ${req.userId}` });
-    }
-
-    res.status(200).json({ tasks });
-  } catch (error) {
-    console.error("Error getting task:", error);
-    res.status(500).json({ message: "Internal Server Error" });
+const getTask = asyncHandler(async (req, res) => {
+  const tasks = await Task.find({ userId: req.userId });
+  if (!tasks.length) {
+    return res
+      .status(404)
+      .json({ message: `No tasks found for ${req.userId}` });
   }
-};
+  res.status(200).json({ tasks });
+  console.error("Error getting task:", error);
+  res.status(500).json({ message: "Internal Server Error" });
+});
 
 //desc Create new task
 //route  post: api/tasks
 //access private
-const createTask = async (req, res) => {
-  try {
-    const { title, description, dueDate, priority, status } = req.body;
+const createTask = asyncHandler(async (req, res) => {
+  const { title, description, dueDate, priority, status } = req.body;
+  // Input validation
+  if (!title || !description || !dueDate || !priority || !status) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
+  // Create a new task
+  const newTask = await Task.create({
+    title,
+    description,
+    dueDate,
+    priority,
+    status,
+    userId: req.userId,
+  });
+  res.status(201).json({ message: "Task created successfully", task: newTask });
+});
 
-    // Input validation
-    if (!title || !description || !dueDate || !priority || !status) {
-      return res.status(400).json({ message: "All fields are required" });
-    }
+//desc  update
+//route  Put: api/tasks/:id
+//access private
+const updateTask = asyncHandler(async (req, res) => {
+  const taskId = req.prams.id;
+  const { title, description, dueDate, priority, status } = req.body;
+  // Input validation
+  if (!title || !description || !dueDate || !priority || !status) {
+    return res.status(400).json({ message: "All fields are required" });
+  }
 
-    // Create a new task
-    const newTask = await Task.create({
+  const updatedTask = await Task.findByIdAndUpdate(
+    taskId,
+    {
       title,
       description,
       dueDate,
       priority,
       status,
-      userId: req.userId,
-    });
-
-    res
-      .status(201)
-      .json({ message: "Task created successfully", task: newTask });
-  } catch (error) {
-    console.error("Error creating task:", error);
-    res.status(500).json({ message: "Internal Server Error" });
-  }
-};
-
-//desc  update
-//route  Put: api/tasks/:id
-//access private
-const updateTask = async (req, res) => {
-  try {
-    const taskId = req.prams.id;
-    const { title, description, dueDate, priority, status } = req.body;
-    // Input validation
-    if (!title || !description || !dueDate || !priority || !status) {
-      return res.status(400).json({ message: "All fields are required" });
+    },
+    {
+      new: true,
     }
-
-    const updatedTask = await Task.findByIdAndUpdate(
-      taskId,
-      {
-        title,
-        description,
-        dueDate,
-        priority,
-        status,
-      },
-      {
-        new: true,
-      }
-    );
-    if (!updatedTask.length) {
-      return res
-        .status(404)
-        .json({ message: `no task found for this user ${taskId}` });
-    }
-    return res.status(200).json({ message: "Successfully updated" });
-  } catch (error) {
-    console.error("Error creating task:", error);
-    res.status(500).json({ message: "Internal Server Error" });
+  );
+  if (!updatedTask.length) {
+    return res
+      .status(404)
+      .json({ message: `no task found for this user ${taskId}` });
   }
-};
+  return res.status(200).json({ message: "Successfully updated" });
+});
 //desc  delete
 //route  delete: api/tasks/:id
 //access private
-const deleteTask = async (req, res) => {
-  try {
-    const deletedTask = await Task.findByIdAndDelete(req.prams.id);
-    if (!deletedTask) {
-      res.status(404).json({
-        message: `No tasks found for this ${req.params.id} to delete`,
-      });
-    }
-    res
-      .status(200)
-      .json({ message: `${req.params.id} is deleted successfully` });
-  } catch (error) {
-    console.error("Error deleting task:", error);
-    res.status(500).json({ message: "Internal Server Error" });
+const deleteTask = asyncHandler(async (req, res) => {
+  const deletedTask = await Task.findByIdAndDelete(req.prams.id);
+  if (!deletedTask) {
+    res.status(404).json({
+      message: `No tasks found for this ${req.params.id} to delete`,
+    });
   }
-};
+  res.status(200).json({ message: `${req.params.id} is deleted successfully` });
+});
 
 module.exports = {
   getTask,
--- Controllers/UserController.js
+++ Controllers/UserController.js
@@ -1,82 +1,72 @@
 const User = require("../model/userModel");
 const bcrypt = require("bcrypt");
-
+const asyncHandler = require("express-async-handler");
 //desc  get all user
 //route get :api/user
 //access private
-const getUser = async (req, res) => {
+const getUser = asyncHandler(async (req, res) => {
   const users = await User.find();
   res.status(200).json(users);
-};
+});
 
 //desc Create new user
 //route  post: api/user
 //access private
-const createUser = async (req, res) => {
+const createUser = asyncHandler(async (req, res) => {
   const { username, email, password } = req.body;
   if (!username || !email || !password) {
     res.status(400).json({ message: "all fields are required" });
   }
-  try {
-    const checkUser = await User.findOne({ email });
+  const checkUser = await User.findOne({ email });
 
-    if (!checkUser) {
-      const hashPassword = await bcrypt.hash(req.body.password, 10);
-      const newUser = await User.create({
-        username: req.body.username,
-        email: req.body.email,
-        password: hashPassword,
-      });
+  if (!checkUser) {
+    const hashPassword = await bcrypt.hash(req.body.password, 10);
+    const newUser = await User.create({
+      username: req.body.username,
+      email: req.body.email,
+      password: hashPassword,
+    });
 
-      res.status(200).json({ message: `successfully created ` });
-    }
-    res.status(403).json({ message: "User already exists" });
-  } catch (error) {
-    console.error("error creating user", error);
+    res.status(200).json({ message: `successfully created ` });
   }
-};
+  res.status(403).json({ message: "User already exists" });
+});
 
 //desc  update user
 //route  Put: api/user/:id
 //access private
-const updateUser = async (req, res) => {
+const updateUser = asyncHandler(async (req, res) => {
   const id = req.params.id;
 
   const { username, email, password } = req.body;
+  const user = await User.findById(id);
 
-  try {
-    const user = await User.findById(id);
-
-    // Check if the user exists
-    if (!user) {
-      return res.status(404).json({ message: "User not found" });
-    }
-
-    // If provided, hash the new password
-    let hashedPassword;
-    if (password) {
-      hashedPassword = await bcrypt.hash(password, 10);
-    }
-
-    // Update the user object
-    if (username) user.username = username;
-    if (email) user.email = email;
-    if (hashedPassword) user.password = hashedPassword;
-
-    // Save the updated user
-    await user.save();
-
-    res.status(200).json({ message: `Updated user successfully ${user}` });
-  } catch (error) {
-    console.error("Error updating user:", error);
-    res.status(500).json({ message: "Internal server error" });
+  // Check if the user exists
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
   }
-};
+
+  // If provided, hash the new password
+  let hashedPassword;
+  if (password) {
+    hashedPassword = await bcrypt.hash(password, 10);
+  }
+
+  // Update the user object
+  if (username) user.username = username;
+  if (email) user.email = email;
+  if (hashedPassword) user.password = hashedPassword;
+
+  // Save the updated user
+  await user.save();
+
+  res.status(200).json({ message: `Updated user successfully ${user}` });
+});
 
 //desc  delete   user
 //route  delete: api/user/:id
 //access private
-const deleteUser = async (req, res) => {
+const deleteUser = asyncHandler(async (req, res) => {
   const id = req.params.id;
   const user = await User.findById(id);
 
@@ -87,7 +77,7 @@ const deleteUser = async (req, res) => {
   await user.deleteOne();
 
   res.status(200).json({ message: `Deleted user with ID ${id}` });
-};
+});
 
 module.exports = {
   getUser,
